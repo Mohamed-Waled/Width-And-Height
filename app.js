a7ed22a93e920894546e76f2c31f6ac10cd24252
Mohamed-Waled <80546573+Mohamed-Waled@users.noreply.github.com>
2022-07-14 21:44:38 +0200

    All Project files

---
+++ app.js
@@ -0,0 +1,7 @@
+let width = document.querySelector(".width");
+let height = document.querySelector(".height");
+
+console.log(window.screen.width)
+
+width.textContent = `The Width Of Your Screen is ${this.screen.width}`;
+height.textContent = `The Height Of Your Screen is ${window.screen.height}`;
